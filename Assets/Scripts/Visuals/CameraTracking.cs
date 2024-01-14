0ea61c8ea79c195f68908b8e2518f745cb138f6e
Jonathan <jacprogrammer@hotmail.com>
2024-01-14 10:42:03 -0800

    Trying to correct multiple CameraTracking scripts

--- Assets/Scripts/Visuals/CameraTracking.cs
+++ Assets/Scripts/Visuals/CameraTracking.cs
@@ -17,15 +17,15 @@ public class CameraTracking : MonoBehaviour
         Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,0f)) - player.position;
         Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(Vector3.zero) - player.position;
 
-        //Debug.DrawRay(player.position, topRight, Color.green, 5f);
-        //Debug.DrawRay(player.position, bottomLeft, Color.green, 5f);
+        Debug.DrawRay(player.position, topRight, Color.green, 5f);
+        Debug.DrawRay(player.position, bottomLeft, Color.green, 5f);
         
         
         horizontal = Vector3.Distance(new Vector3(topRight.x,0f,0f),new Vector3(bottomLeft.x,0f,0f)) / 2;
         vertical = Vector3.Distance(new Vector3(0f,topRight.y,0f),new Vector3(0f,bottomLeft.y,0f)) / 2;
 
-        //Debug.Log(horizontal);
-        //Debug.Log(vertical);
+        Debug.Log(horizontal);
+        Debug.Log(vertical);
     }
 
     // Update is called once per frame
@@ -41,7 +41,7 @@ public class CameraTracking : MonoBehaviour
         pos.z = Mathf.Clamp(pos.z, -10f, -10f);
         //mouseWheelLerpIncrement = Mathf.Lerp(mouseWheelLerpIncrement, Input.GetAxis("Mouse ScrollWheel") * scrollSpeed, 10f * Time.deltaTime);
         //Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - mouseWheelLerpIncrement, zoomOutMin, zoomOutMax);
-        lerpPosition = Vector3.Lerp(lerpPosition, pos, Time.deltaTime * 5);
+        lerpPosition = Vector3.Lerp(lerpPosition, pos, Time.deltaTime);
         transform.position = lerpPosition;
     }
 }
