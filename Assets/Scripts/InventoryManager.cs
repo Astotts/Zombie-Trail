4a1d07cf49ce059e6973773979bd8ed722914974
Jonathan <jacprogrammer@hotmail.com>
2024-01-22 17:02:09 -0800

    Updated InventoryManager
    Added enum to track items through list

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -8,29 +8,40 @@ using UnityEngine;
 /// </summary>
 public class InventoryManager : MonoBehaviour
 {
+    public enum InventoryItem : int
+    {
+        MedicalPack = 0,    // 0
+        Bat,                // 1
+        Pistol,             // 2
+        Shotgun,            // 3
+        AssaultRifle,       // 4
+        AmmoPistol,         // 5
+        AmmoAssaultRifle    // 6
+    }
+
     // Store all the items in the game on this list
     [SerializeField] List<Item> allItemsInGame = new List<Item>();              // Store all the items in the game here, to pull for reference if needed
     [SerializeField] List<Item> equipableItems = new List<Item>();              // Items that has equipable
     [SerializeField] List<Item> playersItems = new List<Item>();                // List to stores the items that the player is carrying
 
-    float maxWeight         = 25f;                              // Max amount of weight the player can carry (start at a low default, and can work up from there for weight carry)
-    float currentWeight     = 0;                                // The amount of weight the player is currently carrying
+    float maxWeight                 = 25f;                              // Max amount of weight the player can carry (start at a low default, and can work up from there for weight carry)
+    float currentWeight             = 0;                                // The amount of weight the player is currently carrying
 
     // Inventory Items section
     // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     // Medical
-    Item medicalPack = new Item("MedicalPack", 5, 0, 1);
+    Item medicalPack                = new Item("MedicalPack", 5, 0, 1);
     // Weapons
-    Item weapon_bat = new Item("Bat", 1, 0, 3.4f);
-    Item weapon_piston = new Item("Pistol", 1, 0, 1.5f);
-    Item weapon_shotgun = new Item("Shotgun", 1, 0, 6);
-    Item weapon_assault_rifle = new Item("Assault Rifle", 1, 0, 7.5f);
+    Item weapon_bat                 = new Item("Bat", 1, 0, 3.4f);
+    Item weapon_piston              = new Item("Pistol", 1, 0, 1.5f);
+    Item weapon_shotgun             = new Item("Shotgun", 1, 0, 6);
+    Item weapon_assault_rifle       = new Item("Assault Rifle", 1, 0, 7.5f);
 
     // Items
-    Item items_ammo_pistol = new Item("Pistol Ammo", 120, 0, .001f);
-    Item items_ammo_assault_rifle = new Item("Assault Rifle Ammo", 120, 0, .001f);
+    Item items_ammo_pistol          = new Item("Pistol Ammo", 120, 0, .001f);
+    Item items_ammo_assault_rifle   = new Item("Assault Rifle Ammo", 120, 0, .001f);
 
     // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -39,17 +50,17 @@ public class InventoryManager : MonoBehaviour
     {
         // Add all the inventory items to the list for the game to use
         // Medical
-        allItemsInGame.Add(medicalPack);
+        allItemsInGame.Add(medicalPack);                // 0
 
         // Weapons
-        allItemsInGame.Add(weapon_bat);
-        allItemsInGame.Add(weapon_piston);
-        allItemsInGame.Add(weapon_shotgun);
-        allItemsInGame.Add(weapon_assault_rifle);
+        allItemsInGame.Add(weapon_bat);                 // 1
+        allItemsInGame.Add(weapon_piston);              // 2
+        allItemsInGame.Add(weapon_shotgun);             // 3
+        allItemsInGame.Add(weapon_assault_rifle);       // 4
 
         // Ammo
-        allItemsInGame.Add(items_ammo_pistol);
-        allItemsInGame.Add(items_ammo_assault_rifle);
+        allItemsInGame.Add(items_ammo_pistol);          // 5
+        allItemsInGame.Add(items_ammo_assault_rifle);   // 6
     }
 
     void AddToInventory(Item itemToAdd)
