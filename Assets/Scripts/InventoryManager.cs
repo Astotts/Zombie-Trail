b0afe7248f20b30f7eb23441f763238f4ad4ef67
Jonathan <jacprogrammer@hotmail.com>
2024-01-22 16:07:24 -0800

    Updated InventoryManager
    Added items to inventory list, with item attributes added.

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -9,41 +9,57 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     // Store all the items in the game on this list
-    List<Item> allItemsInGame = new List<Item>();           // Store all the items in the game here, to pull for reference if needed
-    // Keeping track of the player's inventory
-    List<Item> playersItems = new List<Item>();             // List to stores the items that the player is carrying
-    float maxWeight         = 15;                           // Max amount of weight the player can carry (start at a low default, and can work up from there for weight carry)
-    float currentWeight     = 0;                            // The amount of weight the player is currently carrying
+    [SerializeField] List<Item> allItemsInGame = new List<Item>();           // Store all the items in the game here, to pull for reference if needed
+    [SerializeField] List<Item> playersItems = new List<Item>();             // List to stores the items that the player is carrying
+
+    float maxWeight         = 25f;                              // Max amount of weight the player can carry (start at a low default, and can work up from there for weight carry)
+    float currentWeight     = 0;                                // The amount of weight the player is currently carrying
+
+    // Inventory Items section
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    // Medical
+    Item medicalPack = new Item("MedicalPack", 5, 0, 1);
+    // Weapons
+    Item weapon_bat = new Item("Bat", 1, 0, 3.4f);
+    Item weapon_piston = new Item("Pistol", 1, 0, 1.5f);
+    Item weapon_shotgun = new Item("Shotgun", 1, 0, 6);
+    Item weapon_assault_rifle = new Item("Assault Rifle", 1, 0, 7.5f);
+
+    // Items
+    Item items_ammo_pistol = new Item("Pistol Ammo", 120, 0, .001f);
+    Item items_ammo_assault_rifle = new Item("Assault Rifle Ammo", 120, 0, .001f);
+
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     private void Awake()
     {
         // Add all the inventory items to the list for the game to use
-
         // Medical
-        Item medicalPack = new Item("MedicalPack", 5, 0);
+        allItemsInGame.Add(medicalPack);
+
         // Weapons
-        Item weapon_bat = new Item("Bat", 1, 0);
-        Item weapon_piston = new Item("Pistol", 1, 0);
-        Item weapon_shotgun = new Item("Shotgun", 1, 0);
-        Item weapon_assault_rifle = new Item("Assault Rifle", 1, 0);
+        allItemsInGame.Add(weapon_bat);
+        allItemsInGame.Add(weapon_piston);
+        allItemsInGame.Add(weapon_shotgun);
+        allItemsInGame.Add(weapon_assault_rifle);
 
-        // Items
-        Item items_ammo_pistol = new Item("Pistol Ammo", 120, 0);
-        Item items_ammo_assault_rifle = new Item("Assault Rifle Ammo", 120, 0);
+        // Ammo
+        allItemsInGame.Add(items_ammo_pistol);
+        allItemsInGame.Add(items_ammo_assault_rifle);
     }
-    // Start is called before the first frame update
-    void Start()
+
+    void AddToInventory(Item itemToAdd)
     {
-        
+        // Check if this item can be added, if so, add item
     }
 
-    // Update is called once per frame
-    void Update()
+    void SubtractFromInventory(Item itemToAdd)
     {
-        
+        // Check if this item can be subtracted, if so, subtract item
     }
-
-
 }
 
 /// <summary>
@@ -59,11 +75,13 @@ public class Item : MonoBehaviour
     string itemName = "";       // Item's name
     int maxStorable = 0;        // Max amount storable
     int currentStored = 0;      // The player's current amount of this item on hand
+    float weight = .1f;         // Weight of the item
 
-    public Item(string itemNameParam, int maxStorableParam, int currentStoredParam)
+    public Item(string itemNameParam, int maxStorableParam, int currentStoredParam, float weightOfTheObject)
     {
-        this.itemName = itemNameParam;
-        this.maxStorable = maxStorableParam;
-        this.currentStored = currentStoredParam;
+        this.itemName       = itemNameParam;
+        this.maxStorable    = maxStorableParam;
+        this.currentStored  = currentStoredParam;
+        this.weight         = weightOfTheObject;
     }
 }
