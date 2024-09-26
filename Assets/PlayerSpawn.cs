70260b98e10f58310221f8ff76894d003d5ff39e
Astotts <55721055+Astotts@users.noreply.github.com>
2024-09-25 21:01:21 -0700

    Updated Audio Prefab

---
+++ Assets/PlayerSpawn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    /*[SerializeField] private GameObject spawnPoint;
+    public void override OnNetworkSpawn(){
+
+    }*/
+}
+
+/*you can try to make a player spawn location script
+inherit a networkbehaviour
+then public void override OnNetworkSpawn
+then set location*/
